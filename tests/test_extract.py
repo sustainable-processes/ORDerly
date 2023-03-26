f4a45c38a226173a74202e482636566988cf3d40
joearrowsmith <joearrowsmith98@gmail.com>
2023-03-27 00:28:45 +0100

    adding parameterisation of extraction

--- tests/test_extract.py
+++ tests/test_extract.py
@@ -5,7 +5,15 @@ def test_hello_world():
     assert True
 
 
-def test_extract(tmp_path):
+@pytest.mark.parametrize(
+    "merge_conditions,use_multiprocessing,name_contains_substring", 
+    (
+        [False, True, "uspto"],
+        [True, False, "uspto"],
+        [True, True, None],
+    )
+)
+def test_extract(tmp_path, merge_conditions, use_multiprocessing, name_contains_substring):
 
     pickled_data_folder = tmp_path / "pkl_data"
     pickled_data_folder.mkdir()
@@ -20,12 +28,12 @@ def test_extract(tmp_path):
     orderly.extract.main.main(
         data_path=str(orderly.data.get_path_of_test_ords()),
         ord_file_ending=".pb.gz",
-        merge_conditions=True,
+        merge_conditions=merge_conditions,
         output_path=tmp_path,
         pickled_data_folder=pickled_data_folder,
         molecule_names_folder=molecule_names_folder,
         merged_molecules_file="all_molecule_names.pkl",
-        use_multiprocessing=False,
-        name_contains_substring="uspto",
+        use_multiprocessing=use_multiprocessing,
+        name_contains_substring=name_contains_substring,
         overwrite=True,
     )
