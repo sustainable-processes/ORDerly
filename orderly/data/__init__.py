b0b452ad670d3ba3f6c42d8086eb8a8b0dfb10c4
joearrowsmith <joearrowsmith98@gmail.com>
2023-03-30 20:03:49 +0100

    changing test data

--- orderly/data/__init__.py
+++ orderly/data/__init__.py
@@ -12,9 +12,9 @@ def get_solvents(path: typing.Optional[pathlib.Path] = None) -> pd.DataFrame:
     """reads the solvent csv data stored in the package"""
     if path is None:
         data = pkgutil.get_data("orderly.data", "solvents.csv")
-        return pd.read_csv(io.BytesIO(data), index_col=0)
+        return pd.read_csv(io.BytesIO(data))
 
-    return pd.read_csv(path, index_col=0)
+    return pd.read_csv(path)
 
 
 def get_path_of_test_ords():
