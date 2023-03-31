72f0cff932b773cf653556af2fe711f47a51e74f
joearrowsmith <joearrowsmith98@gmail.com>
2023-03-31 16:18:29 +0100

    adding ord extractor to base path

--- orderly/__init__.py
+++ orderly/__init__.py
@@ -2,6 +2,7 @@ import orderly.clean.cleaner
 import orderly.extract
 import orderly.extract.main
 import orderly.extract.extractor
+from orderly.extract.extractor import OrdExtractor
 import orderly.data
 
 from orderly.types import * # TODO CHANGE TYPES TO USE THE FORMAT orderly.TYPE
