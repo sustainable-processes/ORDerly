e2e8d5b9c300b99a664c8b226b662b0be8ce3857
joearrowsmith <joearrowsmith98@gmail.com>
2023-04-05 18:03:15 +0100

    fixing solvents dict

---
+++ explore.py
@@ -0,0 +1,8 @@
+# %%
+
+
+import orderly
+# %%
+orderly.data.get_solvents_dict()
+
+# %%
--- orderly/data/solvents.py
+++ orderly/data/solvents.py
@@ -13,30 +13,32 @@ def get_solvents(path: typing.Optional[pathlib.Path] = None) -> pd.DataFrame:
     """reads the solvent csv data stored in the package"""
     if path is None:
         data = pkgutil.get_data("orderly.data", "solvents.csv")
-        return pd.read_csv(io.BytesIO(data))
-    return pd.read_csv(path)
+        solvents = pd.read_csv(io.BytesIO(data))
+    else:
+        solvents = pd.read_csv(path)    
 
+    solvents['canonical_smiles'] = solvents["smiles"].apply(orderly.extract.canonicalise.get_canonicalised_smiles)
+    return solvents
 
 def get_solvents_set(path: typing.Optional[pathlib.Path] = None) -> typing.Set[SOLVENT]:
     solvents = orderly.data.get_solvents(path=path)
-    solvents["canonical_smiles"] = solvents["smiles"].apply(
-        orderly.extract.canonicalise.get_canonicalised_smiles
-    )
     return set(solvents["canonical_smiles"])
-
 
 def get_solvents_dict(
     path: typing.Optional[pathlib.Path] = None,
 ) -> typing.Dict[MOLECULE_IDENTIFIER, CANON_SMILES]:
+    # TODO Check when dict is applied we use .lower()
     solvents = orderly.data.get_solvents(path=path)
-    solvents["canonical_smiles"] = solvents["smiles"].apply(
-        orderly.extract.canonicalise.get_canonicalised_smiles
-    )
+    
+    def get_df(name: str, solvents_df: typing.Optional[pd.DataFrame] = None, ) -> pd.DataFrame:
+        if solvents_df is None:
+            solvents_df = orderly.data.get_solvents()
+        else:
+            solvents_df = solvents_df.copy()
+        df = solvents_df[[name, "canonical_smiles"]].dropna().rename({name: "identifer"}, axis=1)
+        df['identifer'] = df['identifer'].str.lower()
+        return df
 
-    # Combine the lists into a sequence of key-value pairs
-    key_value_pairs = zip(
-        list(solvents["stenutz_name"]) + list(solvents["cosmo_name"]),
-        list(solvents["canonical_smiles"]) + list(solvents["canonical_smiles"]),
-    )
-    # Create a dictionary from the sequence
-    return dict(key_value_pairs)
+    return pd.concat([
+        get_df(name=i, solvents_df=solvents) for i in ["stenutz_name", "cosmo_name", "other_name_1", "other_name_2"]
+    ],axis=0).set_index("identifer").to_dict()['canonical_smiles']
