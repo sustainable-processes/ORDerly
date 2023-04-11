7d6482152cd08ccf241023350106755878914c20
joearrowsmith <joearrowsmith98@gmail.com>
2023-04-11 17:50:42 +0100

    changes to log

--- orderly/clean/__main__.py
+++ orderly/clean/__main__.py
@@ -3,12 +3,11 @@ from orderly.clean.cleaner import main_click
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(
-    # filename="cleaning.log",
-    # encoding="utf-8",
+    filename="cleaning.log",
+    encoding="utf-8",
     format="%(name)s - %(levelname)s - %(asctime)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
-    # level=logging.INFO,
-    level=logging.DEBUG,
+    level=logging.INFO,
 )
 
 LOG.info("Running clean main")
