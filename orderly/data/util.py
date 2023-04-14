f7182ffe75c0858e502188ca3ce020610d950764
joearrowsmith <joearrowsmith98@gmail.com>
2023-04-14 11:06:34 +0100

    adding load and save list func

---
+++ orderly/data/util.py
@@ -0,0 +1,14 @@
+from typing import List
+import pathlib
+
+import pandas as pd
+
+def save_list(x: List[str], path: pathlib.Path) -> None:
+    assert isinstance(x, list)
+    for i in x:
+        if not isinstance(i, str):
+            raise TypeError(f"expected a string but got {type(i)=} for {i=}")
+    pd.Series(x, dtype=str).to_csv(path, index=False)
+
+def load_list(path: pathlib.Path) -> List[str]:
+    return pd.read_csv(path).squeeze('columns').tolist()
