fdc206ec1cb76dfaca98cb762807bbb0f05d87ec
joearrowsmith <joearrowsmith98@gmail.com>
2023-04-14 14:44:22 +0100

    regen test data

--- orderly/data/test_data.py
+++ orderly/data/test_data.py
@@ -4,21 +4,25 @@ import pathlib
 
 
 def _get_orderly_data_path() -> pathlib.Path:
-    file_path = sys.modules["orderly.data.test_data"].__file__
+    file_path = sys.modules["orderly.data"].__file__
     if file_path is None:
-        raise ValueError("The path for orderly.data.test_data was not found")
+        raise ValueError("The path for orderly.data was not found")
     return pathlib.Path(os.path.dirname(file_path))
 
 
 def get_path_of_test_ords() -> pathlib.Path:
-    return _get_orderly_data_path() / "ord_test_data"
+    return _get_orderly_data_path() / "test_data" / "ord_test_data"
 
 
 def get_path_of_test_extracted_ords(trust_labelling: bool = False) -> pathlib.Path:
     trust_labelling_str = (
         "_trust_labelling" if trust_labelling else "_dont_trust_labelling"
     )
-    return _get_orderly_data_path() / f"extracted_ord_test_data{trust_labelling_str}"
+    return (
+        _get_orderly_data_path()
+        / "test_data"
+        / f"extracted_ord_test_data{trust_labelling_str}"
+    )
 
 
 def get_path_of_molecule_names(trust_labelling: bool = False) -> pathlib.Path:
@@ -27,6 +31,7 @@ def get_path_of_molecule_names(trust_labelling: bool = False) -> pathlib.Path:
     )
     return (
         _get_orderly_data_path()
+        / "test_data"
         / f"extracted_ord_test_data{trust_labelling_str}"
         / "molecule_names"
     )
