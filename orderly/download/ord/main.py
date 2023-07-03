1a288fd7d4a61001f23db0177fc1fa5f88349318
Daniel Wigh <dswigh@gmail.com>
2023-07-03 15:57:50 +0100

    make black

--- orderly/download/ord/main.py
+++ orderly/download/ord/main.py
@@ -79,17 +79,10 @@ def download_ord(output_control: bool = True, system: Optional[str] = None) -> N
     show_default=True,
     help='controls the system used ("Windows", "Linux", "MacOS")',
 )
-def download_ord_click(
-    output_control: bool,
-    system: str
-):
-
+def download_ord_click(output_control: bool, system: str):
     if system == "default":
         system = None  # type: ignore
-    download_ord(
-        output_control=output_control, 
-        system=system
-    )
+    download_ord(output_control=output_control, system=system)
 
 
 if __name__ == "__main__":
