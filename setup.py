ee620f3591d900b1b87ef71ce508b7ae46bb5dab
Daniel Wigh <dswigh@gmail.com>
2023-07-10 14:52:03 +0100

    changed setup order

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ setup(
     version="0.0.2",
     description="A wrapper for downloading ORD-schema data, extracting and cleaning the data",
     license="MIT",
-    long_description=long_description,
     long_description_content_type="text/markdown",
+    long_description=long_description,
     author=[
         "Daniel S. Wigh <dsw46@cam.ac.uk>",
         "Joe Arrowsmith <joearrowsmith0@gmail.com>",
