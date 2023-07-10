19ef52cd2611e215a8f2477b2e10fde0144b01dc
Daniel Wigh <dswigh@gmail.com>
2023-07-10 12:58:41 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-with open("README", "r") as f:
+with open("README.md", "r") as f:
     long_description = f.read()
 
 setup(
     name="orderly",
-    version="0.0.1",
+    version="0.0.2",
     description="A wrapper for downloading ORD-schema data, extracting and cleaning the data",
     license="MIT",
     long_description=long_description,
@@ -13,6 +13,7 @@ setup(
         "Daniel S. Wigh <dsw46@cam.ac.uk>",
         "Joe Arrowsmith <joearrowsmith0@gmail.com>",
         "Alexander Pomberger <ap2153@cam.ac.uk>",
+        "Kobi C. Felton <kcmf2@cam.ac.uk>",
         "Alexei A. Lapkin <aal35@cam.ac.uk>",
     ],
     packages=["orderly"],
