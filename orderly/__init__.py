6b3c1e0a08412e25c3b2e751d9c915d22d68b15d
Daniel Wigh <dswigh@gmail.com>
2023-07-31 10:19:16 +0100

    comment out the imports in __init__.py

--- orderly/__init__.py
+++ orderly/__init__.py
@@ -1,13 +1,11 @@
-import orderly.clean.cleaner
-import orderly.extract
-import orderly.extract.main
-import orderly.extract.extractor
-from orderly.extract.extractor import OrdExtractor
-import orderly.data
+# import orderly.clean.cleaner
+# import orderly.extract
+# import orderly.extract.main
+# import orderly.extract.extractor
+# from orderly.extract.extractor import OrdExtractor
+# import orderly.data
 
 from orderly.types import *
-
-
 import logging
 
 logging.getLogger("orderly").addHandler(logging.NullHandler())
