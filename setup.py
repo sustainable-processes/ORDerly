762ea0947a42fe2d8c37735a8baea5abd8ae75d6
Daniel Wigh <dswigh@gmail.com>
2023-08-01 15:26:17 +0100

    import orderly.orderly instead of orderly.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="orderly",
-    version="0.1.1",
+    version="0.1.2",
     description="A wrapper for downloading ORD-schema data, extracting and cleaning the data",
     license="MIT",
     long_description_content_type="text/markdown",
